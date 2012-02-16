fca02f332b17c1530316888f9d2101bb1b48fc3f
Francesco Tassi <tassi.francesco@gmail.com>
2012-02-16 16:40:35 +0100

    fixed layout for design elements list

--- web/js/positions.js
+++ web/js/positions.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
   contentList.css({
       'height': $(window).height() - contentListPosition.top,
       'position': 'fixed',
-      'overflow': 'scroll'
+      'overflow': 'auto'
+  })
+  
+  designElementsList = $('#gj_page_admin_design_elements');
+  designElementsListPosition = designElementsList.offset();
+  designElementsList.css({
+      'height': $(window).height() - designElementsListPosition.top,
+      'position': 'fixed',
+      'overflow': 'auto'
   })
 });
 
