a99506ac96f4c596fa775db6a73ad7810bc02fc3
Francesco Tassi <tassi.francesco@gmail.com>
2012-02-16 16:38:39 +0100

    fixed layout for contents list

--- web/js/positions.js
+++ web/js/positions.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   $('.composition-canvas').compositionCanvas();
+  contentList = $('#gj_page_admin_content_elements');
+  contentListPosition = contentList.offset();
+  contentList.css({
+      'height': $(window).height() - contentListPosition.top,
+      'position': 'fixed',
+      'overflow': 'scroll'
+  })
 });
+
+    
